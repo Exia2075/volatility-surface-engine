03b68560314739a268b5c7ec89c47004d0ce3577
Exia2075 <evanxia05@gmail.com>
2026-03-12 16:10:53 -0400

    Add additional liquidity thresholds

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 DEFAULT_RISK_FREE_RATE = 0.05
 DEFAULT_DIVIDEND_YIELD = 0.0
+
 DEFAULT_OPTION_TYPE = "call"
 
 MIN_T_DAYS = 7 # ignore options expiring in less than 1 week
 MAX_T_DAYS = 730 # ignore options expiring in more than 2 yrs
 
 REMOVE_ILLIQUID = True
+MIN_VOLUME = 0
+MIN_OPEN_INTEREST = 0
 
 IV_TOLERANCE = 1e-6
 IV_MAX_ITER = 200
